038bc43192d0413c87255c8d8c00cde071e67d17
Mike Crittenden <mikecrittenden@gmail.com>
2012-12-31 16:51:57 -0500

    working w/ single videos now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,25 +2,25 @@
 
 tooglesApp.controller('MainCtrl', function($scope, $http, $routeParams, $location) {
 
-  $scope.location = $location; // Used for setting active nav.
+  $scope.location = $location;
 
   window.searchCallback = function(data) {
     $scope.videos = data.feed.entry;
+    if ($scope.videos.length === 1) {
+      $scope.embed = $scope.videos[0].media$group.media$content[0].url;
+    }
   }
 
   $scope.search = function() {
     var url = "https://gdata.youtube.com/feeds/api/standardfeeds/recently_featured?max-results=50&alt=json&callback=searchCallback";
     if ($routeParams.query !== undefined) {
-      $scope.query = $routeParams.query;
       var url = "https://gdata.youtube.com/feeds/api/videos?max-results=24&alt=json&q=" + $routeParams.query + "&callback=searchCallback";
     }
     $http.jsonp(url);
   }
 
-  $scope.updateUrl = function() {
-    $location.path('/search/' + $scope.query);
+  if ($location.path().indexOf('search') !== '-1') {
+    // Perform the initial search.
+    $scope.search();
   }
-
-  $scope.search();
 });
-
