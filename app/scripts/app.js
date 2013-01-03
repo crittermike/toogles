f69e6120680fa1a1895e2ab34c4d6bf796a37691
Mike Crittenden <mikecrittenden@gmail.com>
2013-01-03 15:18:18 -0500

    swap out bootstrap with foundation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Boilerplate for Angular
 var tooglesApp = angular.module('tooglesApp', [])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -21,3 +22,28 @@ var tooglesApp = angular.module('tooglesApp', [])
         redirectTo: '/search'
       });
   }]);
+
+// Boilerplate for Foundation
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
+  });
+
+})(jQuery, this);
