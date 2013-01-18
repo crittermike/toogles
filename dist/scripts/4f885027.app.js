7b80bfedeafdc28e80271ea9d6bb33bacb9ef50f
Mike Crittenden <mikecrittenden@gmail.com>
2013-01-18 13:44:14 -0500

    add user pages and rebuild

--- dist/scripts/4f885027.app.js
+++ dist/scripts/4f885027.app.js
@@ -3,11 +3,11 @@
 // Angular setup
 var tooglesApp = angular.module('tooglesApp', ['ngSanitize'])
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/browse', { templateUrl: 'views/search.html', controller: 'ListCtrl' });
-    $routeProvider.when('/browse/:category', { templateUrl: 'views/search.html', controller: 'ListCtrl' });
-    $routeProvider.when('/search/:query', { templateUrl: 'views/search.html', controller: 'ListCtrl' });
+    $routeProvider.when('/browse', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
+    $routeProvider.when('/browse/:category', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
+    $routeProvider.when('/search/:query', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
     $routeProvider.when('/view/:id', { templateUrl: 'views/view.html', controller: 'ViewCtrl' });
-    $routeProvider.when('/user/:username', { templateUrl: 'views/view.html', controller: 'ViewCtrl' });
+    $routeProvider.when('/user/:username', { templateUrl: 'views/user.html', controller: 'UserCtrl' });
     $routeProvider.otherwise({ redirectTo: '/browse' });
   }]);
 
