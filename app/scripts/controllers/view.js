fe7bd5862bc56bf74984067349605d8731bc2cbf
Mike Crittenden <mikecrittenden@gmail.com>
2015-02-03 00:52:18 -0500

    Fix a bunch of bugs - related videos and un-embeddable videos mostly

--- app/scripts/controllers/view.js
+++ app/scripts/controllers/view.js
@@ -13,21 +13,19 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
     if ($scope.section === 'view') {
       $scope.video = data.entry;
       $scope.video.video_id = $routeParams.id;
-      $scope.video.embedurl = "http://www.youtube.com/embed/" + $scope.video.video_id + "?autoplay=1&theme=light&color=white&iv_load_policy=3&origin=http://toogl.es";
     } else {
       var start = $routeParams.start || 0;
       $scope.video = data.feed.entry[start];
       $scope.video.video_id = $scope.video.media$group.yt$videoid.$t;
-      $scope.video.embedurl = "http://www.youtube.com/embed/videoseries?list=" + $routeParams.id + "&autoplay=1&theme=light&color=white&iv_load_policy=3&origin=http://toogl.es&index=" + start;
       $scope.videos = data.feed.entry;
     }
     onYouTubeIframeAPIReady($scope.video.video_id, $scope.section);
     document.title = $scope.video.title.$t + " | Toogles";
-  }
+  };
 
   window.relatedCallback = function(data) {
     $scope.videos = data.feed.entry;
-  }
+  };
 
   $scope.fetchRelated = function() {
     if (!$scope.videos) {
@@ -35,7 +33,7 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
       youtube.getVideos('related', $routeParams.id);
     }
     $scope.showRelated = true;
-  }
+  };
 
   $scope.getLink = function(video, index) {
     if ($scope.section == 'view') {
@@ -43,11 +41,11 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
     } else if ($scope.section = 'playlist') {
       return '#/playlist/' + $routeParams.id + '/' + index
     }
-  }
+  };
 
   $scope.formatDuration = function(seconds) {
     return youtube.formatDuration(seconds);
-  }
+  };
 
   youtube.setCallback('viewCallback');
   if ($scope.section === 'view') {
@@ -60,8 +58,23 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
 
   var onYouTubeIframeAPIReady = function(id, section) {
     var player = new YT.Player('player', {
-      videoId: id,
-      events: {
+      'videoId': id,
+      'playerVars': {
+        'autoplay': 1,
+        'theme': 'light',
+        'color': 'white',
+        'iv_load_policy': 3,
+        'origin': 'http://toogl.es'
+      },
+      'events': {
+        'onError': function(event) {
+          if (event.data == 101 || event.data == 150) {
+            $scope.video.restricted = 1;
+          } else {
+            $scope.video.deleted = 1;
+          }
+          $scope.$apply();
+        },
         'onStateChange': function (event) {
           if (event.data == 1) {
             started = true;
@@ -70,11 +83,23 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
             // When a new video is started in an existing player, open up its dedicated page.
             if (section === 'view') {
               var video_url = event.target.getVideoUrl();
-              var video_id = video_url.replace('http://www.youtube.com/watch?v=', '').replace('&feature=player_embedded', '');
+              var video_id = video_url.replace('http://', '').replace('https://', '').replace('www.youtube.com/watch?v=', '').replace('&feature=player_embedded', '');
               window.location = '#/view/' + video_id;
             } else if (section === 'playlist') {
               window.location = '#/playlist/' + event.target.getPlaylistId() + '/' + event.target.getPlaylistIndex();
             }
+          }
+          else if (started && event.data == 0 && section === 'playlist') {
+            var pathParts = $location.path().split('/');
+            var playlistId = pathParts[2];
+            if (pathParts[3]) {
+              var videoNum = pathParts[3];
+            } else {
+              var videoNum = 0;
+            }
+            videoNum++;
+
+            window.location = '#/playlist/' + playlistId + '/' + videoNum;
           }
         }
       }
